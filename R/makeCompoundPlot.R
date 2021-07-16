578d165de9fa1f76710744be6e7c2bb8c2e24627
Caitlin J Campbell <cjcampbell@users.noreply.github.com>
2021-07-16 15:34:23 -0400

    bug fixes and better documentation
    specify which columns to be called in makeDataEllipse
    replace extra arguments with prop_points
    add parentheses to st_area so that the function actually works

--- R/makeCompoundPlot.R
+++ R/makeCompoundPlot.R
@@ -7,8 +7,9 @@
 #' @param rastnames Character vector of layerNames to be compared (defaults to "Summer" and "Winter" for rast1 and rast2 respectively)
 #' @param species Character vector of species, name of subdirectory where results are (optionally) saved.
 #' @param myEllipses List output of 'makeDataEllipse' function. If not provided, calculations to fit dataEllipse will be rerun. Arguments for makeDataEllipse function should then be provided.
-#' @param csvSaveDir Optional file path to save csv object to.
-#' @param ... DataFrame column nams to populated geom_tile(aes()), e.g., : x=x,y=y,fill=value
+#' @param summaryTable Optional table of values to include under the plot. See output of 'extractStatistics'
+#' @param ... DataFrame column nams to populated "geom_tile(aes())", e.g., : x=x,y=y,fill=value
+#' @param pngSaveDirectory Optional file path to save plot object to.
 #'
 #' @importFrom magrittr %>%
 #' @import ggplot2
@@ -20,12 +21,13 @@
 #'
 #' @seealso makeDataEllipse
 #' @seealso surfaceEquivalency
+#' @seealso extractStatistics
 #'
 #' @return A list containing species name, rastname, metrics of surface equivalency, ellipse areas, ratio of ellipse areas, percent of ellipse intersected, centroid latitude and longitude for each ellipse, and distance and bearing between each centroid.
 #'
 #' @export
 makeCompoundPlot <- function(rast1, rast2, rastnames = c("Summer", "Winter"), myEllipses,
-                             species, summaryTable, pngSaveDirectory = FALSE, ...) {
+                             species, summaryTable = NULL, pngSaveDirectory = FALSE, ...) {
 
   similaritySurface <- schoenersProjection( rast2,rast1, abs = FALSE )
   similaritySurface_df <- surface2df(similaritySurface)
@@ -51,16 +53,22 @@ makeCompoundPlot <- function(rast1, rast2, rastnames = c("Summer", "Winter"), my
       axis.title = element_blank(),
       legend.position = "bottom"
     )
-  mytab <- summaryTable %>%
-    dplyr::rename_all(dplyr::funs(gsub("[[:punct:]]", "\\n", make.names(names(summaryTable))))) %>%
-    dplyr::mutate_if(is.numeric, round, digits = 2) %>%
-    gridExtra::tableGrob(
-      theme = ggplot2::theme_minimal(
-        core = list(fg_params=list(cex = 0.5)),
-        colhead = list(fg_params=list(cex = 0.5)),
-        rowhead = list(fg_params=list(cex = 0.5)))
-    )
-  p2 <- gridExtra::arrangeGrob(p,mytab , nrow = 2, heights = c(5,1))
+
+  if(!is.null(summaryTable)) {
+    mytab <- summaryTable %>%
+      dplyr::rename_all(dplyr::funs(gsub("[[:punct:]]", "\\n", make.names(names(summaryTable))))) %>%
+      dplyr::mutate_if(is.numeric, round, digits = 2) %>%
+      gridExtra::tableGrob(
+        theme = ggplot2::theme_minimal(
+          core = list(fg_params=list(cex = 0.5)),
+          colhead = list(fg_params=list(cex = 0.5)),
+          rowhead = list(fg_params=list(cex = 0.5)))
+      )
+    p2 <- gridExtra::arrangeGrob(p,mytab , nrow = 2, heights = c(5,1))
+  } else {
+    p2 <- gridExtra::arrangeGrob(p)
+  }
+
   if(!isFALSE(pngSaveDirectory)) {
     pngSubdir <- file.path(pngSaveDirectory, species)
     if(!dir.exists(pngSubdir) ) { dir.create(pngSubdir) }
