d194c3ec5d22b2cbf6e93b4d4aa6e1adc8ef7acc
Caitlin J Campbell <cjcampbell@users.noreply.github.com>
2021-07-28 15:25:04 -0400

    auto detect or specify CRS

--- R/makeDataEllipse.R
+++ R/makeDataEllipse.R
@@ -7,6 +7,7 @@
 #' @param weights the value of the surface-as-dataframe, weights the subsampling.
 #' @param n number of sampling replications
 #' @param prop_points what proportion of the subsampled cells should be included in the ellipses
+#' @param crsString string of crs coordinates, should match raster projections.
 #'
 #' @importFrom purrr map
 #' @importFrom sp CRS Polygons Polygon SpatialPolygons
@@ -16,7 +17,7 @@
 #'
 #' @return Returns a list of length 2. First item is a spatialPolygon, the second an sf object.
 #' @export
-makeDataEllipse <- function(coords,  weights, n = 10000, prop_points) {
+makeDataEllipse <- function(coords, crsString, weights, n = 10000, prop_points) {
 
   set.seed(420)
   rownums <- sample(1:nrow(coords), size = n, replace = TRUE, prob = unlist(weights) )
@@ -28,7 +29,7 @@ makeDataEllipse <- function(coords,  weights, n = 10000, prop_points) {
   myEllipse <- list()
   myEllipse$st <- SpatialPolygons(
     list(sp::Polygons(list(with(list(x = ellipsePts[,2], y = ellipsePts[,1]),sp::Polygon(ellipsePts))),1)),
-    proj4string = sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+    proj4string = sp::CRS(crsString)
   )
   myEllipse$sf <- myEllipse$st %>%
     sf::st_as_sf()
@@ -45,18 +46,28 @@ makeDataEllipse <- function(coords,  weights, n = 10000, prop_points) {
 #' @param rast2 Second input rasterLayer
 #' @param n number of sampling replications
 #' @param prop_points what proportion of the subsampled cells should be included in the ellipses
+#' @param crsString string of crs coordinates, should match raster projections.
 #'
+#' @importFrom raster crs
+#' @importFrom purrr map
 #'
 #' @return A list of ellipse objects for rast1 and rast2 respectively
 #'
 #' @export
-makeEllipses <- function(rast1, rast2, prop_points = 0.5, n = 1000) {
+makeEllipses <- function(rast1, rast2, prop_points = 0.5, n = 1000, crsString = FALSE) {
+
+  if(class(rast1) == "RasterLayer" & class(rast2) == "RasterLayer" ) {
+    myCrs <- raster::crs(rast1)
+  } else {
+    if(crsString == FALSE) stop("crsString can't be FALSE if class of rast1 and rast2 are not raster objects")
+    myCrs <- crsString
+  }
 
   if(class(rast1) == "RasterLayer") { rast1 <- surface2df(rast1) }
   if(class(rast2) == "RasterLayer") { rast2 <- surface2df(rast2) }
 
   list( rast1, rast2 ) %>%
     purrr::map(~{
-      makeDataEllipse(coords = .[ ,1:2], weights = .[ ,3], prop_points = prop_points, n = n)
+      makeDataEllipse(coords = .[ ,1:2], weights = .[ ,3], crsString = myCrs, prop_points = prop_points, n = n)
     })
 }
