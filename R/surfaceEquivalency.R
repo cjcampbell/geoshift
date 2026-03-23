8d510e5e7df805628de3eb859046f995bd55eed1
Caitlin J Campbell <cjcampbell@users.noreply.github.com>
2026-03-23 13:36:16 -0400

    update to terra's raster-equivalent functions

--- R/surfaceEquivalency.R
+++ R/surfaceEquivalency.R
@@ -1,46 +1,46 @@
-#' Compare two rasterLayer surfaces.
+#' Compare two raster surfaces.
 #'
-#' Functions to compare rasterLayers using Schoener's D-metric.
+#' Functions to compare rasters using Schoener's D-metric.
 #'
-#' The function schoenersD' calculates similarity value of two RasterLayers
+#' The function schoenersD' calculates similarity value of two spatRaster
 #' using Schoener's D-metric.
 #'
 #' The function 'schoenersProjection' returns a third rasterLayer with the
 #' cell-by-cell difference between surfaces.
 #'
 #'
-#' RasterLayers must have identical resolutions and extents. Schoener's D assumes
+#' SpatRasters must have identical resolutions and extents. Schoener's D assumes
 #' surfaces each sum to 1.
 #'
 #' @rdname surfaceSimilarity
-#' @param rast1 First input rasterLayer
-#' @param rast2 Second input rasterLayer
+#' @param rast1 First input SpatRaster
+#' @param rast2 Second input SpatRaster
 #'
-#' @importFrom raster cellStats
+#' @importFrom terra global
 #'
 #' @export
 schoenersD <- function(rast1, rast2) {
-  if (class(rast1) != "RasterLayer" | class(rast2) != "RasterLayer")
-    stop("arguments are not of class 'RasterLayer'")
+  if (class(rast1) != "SpatRaster" | class(rast2) != "SpatRaster")
+    stop("arguments are not of class 'SpatRaster'")
 
-  r1 <- rast1/raster::cellStats(rast1, stat = 'sum')
-  r2 <- rast2/raster::cellStats(rast2, stat = 'sum')
+  r1 <- rast1/{ terra::global(rast1, fun = 'sum', na.rm = T)[[1]]}
+  r2 <- rast2/{ terra::global(rast2, fun = 'sum', na.rm = T)[[1]]}
 
-  1 - (0.5 * raster::cellStats(abs(r1 - r2), stat = "sum"))
+  1 - (0.5 * {terra::global(abs(r1 - r2), fun = "sum", na.rm = T)[[1]]})
 }
 
 #' @rdname surfaceSimilarity
 #'
-#' @param rast1 First input rasterLayer
-#' @param rast2 Second input rasterLayer
+#' @param rast1 First input spatRaster
+#' @param rast2 Second input spatRaster
 #' @param abs Specify whether to return the absolute or relative change between rast1 and rast2
 #'
 #' @export
 schoenersProjection <- function(rast1, rast2, abs = TRUE) {
-  #
-  r1 <- rast1/raster::cellStats(rast1, stat = 'sum')
-  r2 <- rast2/raster::cellStats(rast2, stat = 'sum')
-  #
+
+  r1 <- rast1/{ terra::global(rast1, fun = 'sum', na.rm = T)[[1]]}
+  r2 <- rast2/{ terra::global(rast2, fun = 'sum', na.rm = T)[[1]]}
+
   if(abs == TRUE){
     out <- 1 - (0.5*abs(r1 - r2))
   } else {
